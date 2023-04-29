4fb1815bacc8d25708c55079c96e466fca7a86a0
Daniyal Khalid <syed.daniyal@joyn.de>
2023-04-30 00:59:25 +0200

    add ingestion and transform

---
+++ beam/main.py
@@ -0,0 +1,103 @@
+import argparse
+import json
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, StandardOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.io import ReadFromPubSub, WriteToBigQuery
+from textblob import TextBlob
+from langdetect import detect
+
+
+class DeserializeJson(beam.DoFn):
+    def process(self, element):
+        try:
+            return [json.loads(element)]
+        except json.JSONDecodeError:
+            return []
+        
+
+class DeduplicateURL(beam.PTransform):
+    def expand(self, pcoll):
+        return (
+            pcoll
+            | "Key by URL" >> beam.WithKeys(lambda x: x["url"])
+            | "Windowing" >> beam.WindowInto(beam.window.FixedWindows(1000000),
+                                               trigger=beam.trigger.Repeatedly(
+                                                   beam.trigger.AfterCount(1)),
+                                               accumulation_mode=beam.trigger.AccumulationMode.DISCARDING)
+            | "Group by URL" >> beam.GroupByKey()
+            | "Take first element of grouped URLs" >> beam.Map(lambda x: x[1][0])
+        )
+
+class AddSentimentScore(beam.DoFn):
+    def process(self, element):
+        text = element['title'] + ' ' + element['description']
+        sentiment_score = TextBlob(text).sentiment.polarity
+        element['sentiment_score'] = sentiment_score
+        return [element]
+
+
+class AddWordCount(beam.DoFn):
+    def process(self, element):
+        text = element['title'] + ' ' + element['description']
+        word_count = len(text.split())
+        element['word_count'] = word_count
+        return [element]
+
+
+class AddLanguage(beam.DoFn):
+    def process(self, element):
+        try:
+            language = detect(element['title'])
+        except:
+            language = 'unknown'
+        element['language'] = language
+        return [element]
+
+
+def run(argv=None, save_main_session=True):
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--input_topic',
+        help='The Cloud Pub/Sub topic to read from.',
+        required=True)
+    parser.add_argument(
+        '--output_table',
+        help='The BigQuery table to write the results to.',
+        required=True)
+    known_args, pipeline_args = parser.parse_known_args(argv)
+
+    pipeline_options = PipelineOptions(pipeline_args)
+    google_cloud_options = pipeline_options.view_as(GoogleCloudOptions)
+    pipeline_options.view_as(beam.options.pipeline_options.StandardOptions).streaming = True
+    pipeline_options.view_as(StandardOptions).runner = 'DataflowRunner'
+
+    with beam.Pipeline(options=pipeline_options) as p:
+        (p | 'ReadFromPubSub' >> ReadFromPubSub(topic=known_args.input_topic)
+           | 'DeserializeJson' >> beam.ParDo(DeserializeJson())
+           | 'Deduplicate by URL' >> DeduplicateURL()
+           | 'AddSentimentScore' >> beam.ParDo(AddSentimentScore())
+           | 'AddWordCount' >> beam.ParDo(AddWordCount())
+           | 'AddLanguage' >> beam.ParDo(AddLanguage())
+           | 'WriteToBigQuery' >> WriteToBigQuery(
+               known_args.output_table,
+               schema='source_id:STRING, source_name:STRING, author:STRING, '
+                      'title:STRING, description:STRING, url:STRING, '
+                      'urlToImage:STRING, publishedAt:STRING, content:STRING, '
+                      'sentiment_score:FLOAT, word_count:INT64, language:STRING',
+               write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND))
+
+
+if __name__ == '__main__':
+    run()
+
+
+'''
+python main.py \
+  --input_topic "projects/$GCP_PROJECT_ID/topics/$INPUT_TOPIC" \
+  --output_table "$GCP_PROJECT_ID:$GCP_RAW_TABLE" \
+  --project $GCP_PROJECT_ID \
+  --region $GCP_REGION \
+  --zone europe-west3-a
+  --temp_location gs://news-data-pipeline-processing/temp
+'''
---
+++ ingest/main.py
@@ -0,0 +1,91 @@
+import argparse
+import os
+import json
+import requests
+import datetime
+from google.cloud import pubsub_v1
+
+API_KEY = os.environ.get('NEWS_API_KEY')
+NEWS_API_URL = 'https://newsapi.org/v2/everything'
+PUBSUB_PROJECT_ID = os.environ.get('GCP_PROJECT_ID')
+PUBSUB_TOPIC = os.environ.get('GCP_PUBSUB_TOPIC')
+LAST_RUN_FILE = 'last_run.txt'
+
+def save_last_run_time(timestamp):
+    with open(LAST_RUN_FILE, 'w') as file:
+        file.write(str(timestamp))
+
+def load_last_run_time():
+    if os.path.exists(LAST_RUN_FILE):
+        with open(LAST_RUN_FILE, 'r') as file:
+            return datetime.datetime.fromisoformat(file.read()).strftime("%Y-%m-%d")
+    return None
+
+
+def fetch_articles(keywords, from_time):
+    query = f'{" OR ".join(keywords)}'
+    params = {
+        'apiKey': API_KEY,
+        'q': query,
+        'from': from_time,
+        'sortBy': 'publishedAt',
+        'pageSize': 100
+    }
+    print(params)
+    all_articles = []
+    page = 1
+    MAX_PAGES = 1
+    c = 0
+    while c < MAX_PAGES:
+        c += 1
+        params['page'] = page
+        response = requests.get(NEWS_API_URL, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            all_articles.extend(data['articles'])
+            if page * 100 >= data['totalResults']:
+                break
+            page += 1
+        else:
+            print(f"Error {response.status_code}: {response.text}")
+            break
+    return all_articles
+
+
+def create_pubsub_publisher():
+    return pubsub_v1.PublisherClient()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("keywords", help="Comma-separated keywords to fetch articles")
+    args = parser.parse_args()
+    keywords = args.keywords.split(',')
+
+    last_run_time = load_last_run_time()
+    if last_run_time is None:
+        last_run_time = (datetime.datetime.now() - datetime.timedelta(hours=24)).strftime("%Y-%m-%d")
+
+    articles = fetch_articles(keywords, last_run_time)
+
+    publisher = create_pubsub_publisher()
+    topic_path = publisher.topic_path(PUBSUB_PROJECT_ID, PUBSUB_TOPIC)
+    
+    print(articles)
+
+    print("GONNA PUBLISH NOW")
+    for article in articles:
+        data = json.dumps(article).encode('utf-8')
+        future = publisher.publish(topic_path, data=data)
+        message_id = future.result()
+        print(f"Published message ID: {message_id}")
+
+    
+
+
+    save_last_run_time(datetime.datetime.now())
+
+
+
+if __name__ == '__main__':
+    main()
